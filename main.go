6f38f196b6389752b5ae358a7bfd5425acaa3afc
Amadou BA <baamadou947@gmai.com>
2026-05-27 14:21:36 +0000

    chap-4 L-7: Passing varibles by value

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+func monthlyBillIncrease(costPerSend, numLastMonth, numThisMonth int) int {
+	var lastMonthBill int
+	var thisMonthBill int
+	lastMonthBill = getBillForMonth(costPerSend, numLastMonth)
+	thisMonthBill = getBillForMonth(costPerSend, numThisMonth)
+	return thisMonthBill - lastMonthBill
+}
 
-func main() {
-    fmt.Println("Hello, World!")
+func getBillForMonth(costPerSend, messagesSent int) int {
+	return costPerSend * messagesSent
 }
