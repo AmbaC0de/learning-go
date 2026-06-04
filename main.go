4c99de8a509d713f0ab97aeec199d510803c22b2
Amadou BA <baamadou947@gmai.com>
2026-06-04 14:18:18 +0000

    chap4 L-15 functions as values

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
-func yearsUntilEvents(age int) (yearsUntilAdult, yearsUntilDrinking, yearsUntilCarRental int) {
-	yearsUntilAdult = 18 - age
-	if yearsUntilAdult < 0 {
-		yearsUntilAdult = 0
-	}
-	yearsUntilDrinking = 21 - age
-	if yearsUntilDrinking < 0 {
-		yearsUntilDrinking = 0
-	}
-	yearsUntilCarRental = 25 - age
-	if yearsUntilCarRental < 0 {
-		yearsUntilCarRental = 0
-	}
-	return
+func reformat(message string, formatter func(string) string) string {
+	firstResult := formatter(message)
+	secondResult := formatter(firstResult)
+	thirdResult := formatter(secondResult)
+
+	return "TEXTIO: " + thirdResult
 }
